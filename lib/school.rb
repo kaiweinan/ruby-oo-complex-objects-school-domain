35c733357a902ba0e0443b85d74436e2b830b40e
kaiweinan <kaiweinan@yahoo.com>
2021-01-07 21:30:34 -0500

    commit tree f0895d038556017da84a1917c200645a0f61881a

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+class School
+    attr_accessor :roster, :name, :grade
+    
+    def initialize(name)
+      @name = name
+      @roster = {}
+    end
+  
+    def add_student(student_name, grade)
+      @roster[grade] ||= []
+      @roster[grade] << student_name
+    end
+  
+    def grade(grade)
+      @roster.select { |g, student_hash| return student_hash if g == grade}
+    end
+  
+    def sort
+      sorted_hash = {}
+      self.roster.sort.each do |grade_array|
+          sorted_hash[grade_array[0]] = grade_array[1].sort
+      end
+      sorted_hash
+    end   
+  end
